7118b6d8b77cac7627112a4a8af93393d1c028aa
Tom Durrant <t.durrant@oceanum.science>
2024-07-31 14:58:35 +1000

    Added settings.py

---
+++ rompy/settings.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+# Define datasource types to be used. Note that including SourceDataset will break the
+# specification as this object cannot be serialised, but this does not cause any problems
+# working in a python environement
+DATA_SOURCE_TYPES = os.getenv(
+    "DATA_SOURCE_TYPES",
+    "SourceDataset:rompy.core.data,SourceFile:rompy.core.data,SourceIntake:rompy.core.data,SourceDatamesh:rompy.core.data",
+)
+
+BOUNDARY_SOURCE_TYPES = os.getenv("BOUNDARY_SOURCE_TYPES", DATA_SOURCE_TYPES)
+SPEC_BOUNDARY_SOURCE_TYPES = os.getenv(
+    "SPEC_BOUNDARY_SOURCE_TYPES",
+    DATA_SOURCE_TYPES + ",SourceWavespectra:rompy.core.boundary",
+)
